718944a61e0ec5481fcd57c41c46eb01d35f0f9e
Ramandeep Malhotra <ramandeep.malhotra@nagarro.com>
2019-08-29 18:44:32 +0530

    Updates

---
+++ precache-manifest.6943f53003ac7665252d201909fb46e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9970297f54d778b5a9e7c13a34ab6b26",
+    "url": "/AAPassignment/index.html"
+  },
+  {
+    "revision": "0ea78fa21a0ef9090ae1",
+    "url": "/AAPassignment/static/css/main.03703fa4.chunk.css"
+  },
+  {
+    "revision": "fa65c06de2b6aff52c1a",
+    "url": "/AAPassignment/static/js/2.c3bce265.chunk.js"
+  },
+  {
+    "revision": "0ea78fa21a0ef9090ae1",
+    "url": "/AAPassignment/static/js/main.e712ca82.chunk.js"
+  },
+  {
+    "revision": "bef7ea40a1f2548c0b68",
+    "url": "/AAPassignment/static/js/runtime~main.a884dcb9.js"
+  }
+]);
